b35f6369135e8d416f5a3596e68edb6118bbf2c3
grt1st <grt1stnull@gmail.com>
2018-07-24 20:09:35 +0800

    add sql section

--- platform/web.go
+++ platform/web.go
@@ -4,6 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"github.com/gin-contrib/multitemplate"
+	"github.com/grt1st/wdproxy/g"
+	"time"
+	"fmt"
+	"encoding/json"
 )
 
 func RunWebPlatform() {
@@ -35,9 +39,15 @@ func createRender() multitemplate.Renderer {
 }
 
 func Index(c *gin.Context) {
+	var numDomains, numRequests, numTags int
+	g.DB.Model(g.WdproxyRecord{}).Count(&numRequests)
+	g.DB.Model(g.WdproxyDomain{}).Count(&numDomains)
 	c.HTML(http.StatusOK, "index", gin.H{
 		"title": "heatmap",
 		"home": "active",
+		"num_domains": numDomains,
+		"num_requests": numRequests,
+		"num_tags": numTags,
 	})
 }
 
@@ -49,22 +59,47 @@ func Login(c *gin.Context) {
 }
 
 func Settings(c *gin.Context) {
+	var num24h, numAll int
+	t := time.Now()
+	g.DB.Model(g.WdproxyRecord{}).Count(&numAll)
+	g.DB.Model(g.WdproxyRecord{}).Where("created_at > ?", t.AddDate(0,0,-1)).Count(&num24h)
 	c.HTML(http.StatusOK, "settings", gin.H{
 		"title": "heatmap",
 		"settings": "active",
+		"check": "yes",
+		"num_24h": num24h,
+		"num_all": numAll,
 	})
 }
 
 func Dashboard(c *gin.Context) {
+	var records []g.WdproxyRecord
+	g.DB.Model(g.WdproxyRecord{}).Find(&records)
 	c.HTML(http.StatusOK, "dashboard", gin.H{
 		"title": "heatmap",
 		"dashboard": "active",
+		"records": records,
 	})
 }
 
+type WdproxyRecord struct {
+	g.WdproxyRecord
+	HeaderRequestJson   map[string]interface{}
+	HeaderResponseJson  map[string]interface{}
+}
+
 func Detail(c *gin.Context) {
+	id := c.Query("id")
+	var record WdproxyRecord
+	err := g.DB.Where("id = ?", id).First(&record).Error
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = json.Unmarshal([]byte(record.HeaderRequest), &record.HeaderRequestJson)
+	err = json.Unmarshal([]byte(record.HeaderResponse), &record.HeaderResponseJson)
 	c.HTML(http.StatusOK, "detail", gin.H{
 		"title": "heatmap",
 		"dashboard": "active",
+		"record": record,
 	})
 }
